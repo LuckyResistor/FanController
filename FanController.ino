b9987afe0692e828f5903fff7ab49fb8b0a9ae76
Lucky Resistor <contact-me@luckyresistor.me>
2016-10-06 22:20:05 +0200

    Add various features to the fan controller code.
    - Add custom characters to make the display more interesting.
    - Display the fan speed using a bar.
    - Add a manual mode where the user can control the fan speed without PID.

--- FanController.ino
+++ FanController.ino
@@ -29,17 +29,48 @@
 
 
 // Constants
-const uint8_t cFanControlPin = 5;
-const uint8_t cTemperatureSensor1Pin = 2;
-const uint8_t cTemperatureSensor2Pin = 4;
-const uint8_t cSdCardChipSelectPin = 10;
-const uint16_t cUpdateDelay = 2200; // Update every ~2.2 seconds.
+const uint8_t cFanControlPin = 5; // The pin where the fan PWM is connected.
+const uint8_t cTemperatureSensor1Pin = 2; // The pin for the first temperature sensor data.
+const uint8_t cTemperatureSensor2Pin = 4; // The pin for the second temperature sensor data.
+const uint8_t cSdCardChipSelectPin = 10; // The pin where the SD card chip select is connected.
+const uint16_t cUpdateDelay = 2200; // The update every ~2.2 seconds.
+const uint8_t cStartFanSpeed = 0x80; // The initial fan speed.
+const uint8_t cControlTargetTemperature = 30; // The maximum target temperature.
 
-// Control constants
-const uint8_t cControlMinimumTemperature = 33;
-const uint8_t cControlMinimumFanSpeed = 0xa0;
-const uint8_t cControlMaximumTemperature = 38;
-const uint8_t cControlMaximumFanSpeed = 0xff;
+// Custom characters for the LCD display.
+const uint8_t cFanCharacterMask[] PROGMEM = {
+  0b01100,
+  0b01100,
+  0b00101,
+  0b11011,
+  0b11011,
+  0b10100,
+  0b00110,
+  0b00110,
+};
+const char cFanCharacter = '\x05';
+const uint8_t cTemp1CharacterMask[] PROGMEM = {
+  0b01000,
+  0b10100,
+  0b10100,
+  0b10100,
+  0b10100,
+  0b11101,
+  0b11101,
+  0b01000
+};
+const char cTemp1Character = '\x06';
+const uint8_t cTemp2CharacterMask[] PROGMEM = {
+  0b01000,
+  0b10101,
+  0b10101,
+  0b10100,
+  0b10100,
+  0b11101,
+  0b11101,
+  0b01000
+};
+const char cTemp2Character = '\x07';
 
 
 // The instance to access the LCD display.
@@ -54,27 +85,42 @@ RTC_DS1307 rtc;
 
 
 // The current fan speed.
-uint8_t fanSpeed = 0xa0;
+uint8_t fanSpeed = cStartFanSpeed;
 
-// The current temperatures.
-float temperature1 = 0;
-float temperature2 = 0;
-
-// The PID algorithm
+// The PID controller instance.
 lr::PIDController pidController;
 
 
-// Setup everything.
+/// Setup everything.
+///
 void setup() {
+  // Set the pin for the fan PWM control.
+  pinMode(cFanControlPin, OUTPUT);
+  analogWrite(cFanControlPin, 0x00);
+
   // Initialize the serial output.
   Serial.begin(57600);
-  while (!Serial) { _NOP(); }
+  while (!Serial) { _NOP(); } // Wait until it is ready.
   Serial.println(F("Starting Log Control"));
   
   // Setup the LCD display.
   lcd.begin(16, 2);
   lcd.setBacklight(0x7);
-
+  // Add custom characters for the fan as bar display.
+  uint8_t bars[] = {B10000, B11000, B11100, B11110, B11111};
+  uint8_t character[8];
+  for (uint8_t i = 0; i < 5; ++i) {
+    for (uint8_t j = 0; j < 8; ++j) character[j] = bars[i];
+    lcd.createChar(i, character);
+  }
+  // Add custom characters from the masks.
+  memcpy_P(character, cFanCharacterMask, 8);
+  lcd.createChar(cFanCharacter, character);
+  memcpy_P(character, cTemp1CharacterMask, 8);
+  lcd.createChar(cTemp1Character, character);
+  memcpy_P(character, cTemp2CharacterMask, 8);
+  lcd.createChar(cTemp2Character, character);
+  
   // Setup the temperature sensors
   temperatureSensor1.begin();
   temperatureSensor2.begin();
@@ -85,7 +131,7 @@ void setup() {
   pidController.setOutputReverse(true);
   pidController.setOutputDrift(-0.05f);
   pidController.setOutputValue(fanSpeed);
-  pidController.setTargetValue(32.0f);
+  pidController.setTargetValue(30.0f);
 
   // Initialize the SD card library
   if (!SD.begin(cSdCardChipSelectPin)) {
@@ -95,10 +141,7 @@ void setup() {
     lcd.clear();
   }
 
-  // Set the pin for the FAN control.
-  pinMode(cFanControlPin, OUTPUT);
-
-  // Write some welcome message.
+  // Write some welcome message and wait 2 seconds.
   lcd.print(F(" Fan Controller"));
   lcd.setCursor(0, 1);
   lcd.print(F("    Welcome!"));
@@ -107,7 +150,10 @@ void setup() {
 }
 
 
-// Print a decimal number padded with an optional zero to the LCD.
+/// Print a decimal number padded with an optional zero to the LCD.
+///
+/// @param value The value to display.
+///
 void lcdPrintPaddedDecimal(uint8_t value) {
   if (value < 10) {
     lcd.print('0');
@@ -116,34 +162,92 @@ void lcdPrintPaddedDecimal(uint8_t value) {
 }
 
 
-// All measurements are done every 10 seconds.
-void loop() {
-  // Get the current time
-  DateTime now = rtc.now();
-  
-  // First measure the current temperatures.
-  temperature1 = temperatureSensor1.readTemperature();
-  temperature2 = temperatureSensor2.readTemperature();
+/// A special loop for manual control mode.
+///
+void manualLoop() {
+  uint8_t displayUpdateDelay = 0; // A delay to keep the update delay.
+  bool manualDisplay = false; // A flag to switch between the "manual" message and the date/time
+  uint8_t lastButtonState = 0; // The last button state for comparision
+  // Loop until "break".
+  while (true) {
+    // Get the current button state.
+    const uint8_t currentButtonState = lcd.readButtons();
+    // Check which buttons got a press.
+    const uint8_t pressedButtons = (lastButtonState ^ currentButtonState) & currentButtonState;
+    // If left increase the fan speed.
+    if (pressedButtons & BUTTON_LEFT) {
+      if (fanSpeed < 0x10) {
+        fanSpeed = 0;
+      } else {
+        fanSpeed -= 0x10;
+      }
+      analogWrite(cFanControlPin, fanSpeed);
+      displayUpdateDelay = 0; // force display update.
+    } else if (pressedButtons & BUTTON_RIGHT) { // if right decrease the fan speed.
+      if (fanSpeed >= 0xf0) {
+        fanSpeed = 0xff;
+      } else {
+        fanSpeed += 0x10;
+      }
+      analogWrite(cFanControlPin, fanSpeed);
+      displayUpdateDelay = 0; // force display update.
+    } else if (pressedButtons & BUTTON_DOWN) { // if down, exit this loop.
+      pidController.setOutputValue(fanSpeed); // Make sure the PID controller knows the new fan speed.
+      break;
+    }
+    lastButtonState = currentButtonState; // Store the current button state for the next loop.
+    // Every few seconds, refresh the display.
+    if (displayUpdateDelay == 0) {
+        // Get the current time.
+        DateTime now = rtc.now();
+        // Measure the current temperatures.
+        const float temperature1 = temperatureSensor1.readTemperature();
+        const float temperature2 = temperatureSensor2.readTemperature();
+        // Build the display.
+        updateTemperatureDisplay(temperature1, temperature2);
+        updateFanSpeedDisplay();
+        // Switch between the "manual control" message and the date/time display.
+        if (manualDisplay) {
+          lcd.setCursor(0, 1);
+          lcd.print(F(" Manual Control "));
+        } else {
+          updateTimeDisplay(now);
+        }
+        manualDisplay = !manualDisplay;
+    }
+    // Make sure the display is refreshed as specified in cUpdateDelay.
+    if (++displayUpdateDelay >= (cUpdateDelay/100)) {
+      displayUpdateDelay = 0;
+    }
+    delay(100);
+  }
+}
 
-  // Calculate the new fan speed.
-  const float maximumTemperature = max(temperature1, temperature2);
-  fanSpeed = static_cast<uint8_t>(pidController.calculateOutput(maximumTemperature));
-  analogWrite(cFanControlPin, fanSpeed);
-  
-  // Build the display
+
+/// Update the temperature display
+///
+/// @param temperature1 The first temperature value in degree celsius.
+/// @param temperature2 The second temperature value in degree celsius.
+///
+void updateTemperatureDisplay(float temperature1, float temperature2) {
   lcd.setCursor(0, 0);
-  lcd.print(F("1:"));
+  lcd.print(cTemp1Character);
   lcd.print(static_cast<uint8_t>(round(temperature1)), DEC);
   lcd.print('\xdf');
   lcd.print('C');
   lcd.print(' ');
-  lcd.print(F("2:"));
+  lcd.print(cTemp2Character);
   lcd.print(static_cast<uint8_t>(round(temperature2)), DEC);
   lcd.print('\xdf');
   lcd.print('C');
-  lcd.print(' ');
-  lcd.print('F');
-  lcd.print(static_cast<char>('0' + (fanSpeed/26)));
+}
+
+
+/// Update the time display.
+///
+/// @param now The time to display.
+///
+void updateTimeDisplay(const DateTime &now) {
   lcd.setCursor(0, 1);
   lcd.print(now.year(), DEC);
   lcd.print('-');
@@ -153,16 +257,87 @@ void loop() {
   lcd.print(' ');
   lcdPrintPaddedDecimal(now.hour());
   lcd.print(':');
-  lcdPrintPaddedDecimal(now.minute());
+  lcdPrintPaddedDecimal(now.minute());  
+}
 
+
+/// Print a single bar character of the given value (0-5).
+///
+/// @param value The bar value to display (0-5).
+///
+void lcdPrintBar(const uint8_t value) {
+  if (value == 0) {
+    lcd.print(' ');
+  } else {
+    lcd.print(static_cast<char>(value-1));
+  }
+}
+
+
+/// Update the fan speed display.
+///
+void updateFanSpeedDisplay() {
+  lcd.setCursor(12, 0);
+  lcd.print(cFanCharacter);
+  const uint8_t displayedValue = (fanSpeed>>4);
+  if (displayedValue > 10) {
+    lcdPrintBar(5);
+    lcdPrintBar(5);
+    lcdPrintBar(displayedValue - 10);
+  } else if (displayedValue > 5) {
+    lcdPrintBar(5);
+    lcdPrintBar(displayedValue - 5);
+    lcdPrintBar(0);
+  } else {
+    lcdPrintBar(displayedValue);
+    lcdPrintBar(0);
+    lcdPrintBar(0);
+  }
+}
+
+
+/// The main loop of the controller
+///
+void loop() {
+
+  // Check if manual mode is requested.
+  const uint8_t buttonState = lcd.readButtons();
+  if (buttonState & BUTTON_UP) {
+    manualLoop(); // go to manual loop, until left back to this loop.
+  }
+
+  // Get the current time from the RTC.
+  DateTime now = rtc.now();
+
+  // First measure the current temperatures.
+  const float temperature1 = temperatureSensor1.readTemperature();
+  const float temperature2 = temperatureSensor2.readTemperature();
+
+  // Calculate the new fan speed using the PID controller.
+  const float maximumTemperature = max(temperature1, temperature2);
+  fanSpeed = static_cast<uint8_t>(pidController.calculateOutput(maximumTemperature));
+  analogWrite(cFanControlPin, fanSpeed);
+  
+  // Refresh the display.
+  updateTemperatureDisplay(temperature1, temperature2);
+  updateFanSpeedDisplay();
+  updateTimeDisplay(now);
+
+  // Write all values into a log file on the SD card.
   writeLogEntry(now, temperature1, temperature2, fanSpeed);
 
-  // Wait 5 seconds for the next measurement.
+  // Wait "cUpdateDelay" miliseconds for the next measurement.
   delay(cUpdateDelay);
 }
 
 
-// Write a single log entry.
+/// Write a single log entry.
+///
+/// @param time The time to write.
+/// @param temperature1 The first temperature to write.
+/// @param temperature2 The second temperature to write.
+/// @param fanSpeed The fan speed to write.
+///
 void writeLogEntry(const DateTime &time, const float temperature1, const float temperature2, uint8_t fanSpeed) {
   // Generate the log file name with the date and time.
   char *fileName = new char[32]; // temperature_yyyy-mm-dd.log
@@ -193,11 +368,11 @@ void writeLogEntry(const DateTime &time, const float temperature1, const float t
     logFile.println(line);
     logFile.close();
   } else {
-    // Display a ! character to signal a problem with log writing.
-    lcd.setCursor(13, 0);
+    // Display a ! character on the display to signal a problem with log writing.
+    lcd.setCursor(11, 0);
     lcd.print('!');
   }
-  // Send it to serial.
+  // Send the same log line to the serial line.
   Serial.println(line);
 }
 
